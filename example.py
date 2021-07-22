af5e557f7845770d5bd667eac48e3b6cd456d1da
Christopher Angel <cangel@uark.edu>
2021-07-22 17:15:09 -0500

    Add sentinel 2 example for searching and storing based on spatial and temporal filters

--- example.py
+++ example.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from datetime import date
 from usgs import Api
 
-from usgs.filters import AcquisitionFilter, SceneFilter
+from usgs.filters import AcquisitionFilter, SceneFilter, SpatialMbr
 
 
 if __name__ == "__main__":
     # Load env variables
     load_dotenv()
+
+    primary_dataset = "sentinel_2a"
 
     # Instantiate the Usgs API
     api = Api()
@@ -15,18 +17,25 @@ if __name__ == "__main__":
     # Create a Scene filter with an acquisition filter
     scene_filter = SceneFilter(
         acquisitionFilter=AcquisitionFilter(
-            start=date(1972, 5, 24), end=date(1972, 5, 31)
-        )
+            start=date(2019, 5, 24), end=date(2019, 5, 31)
+        ),
+        spatialFilter=SpatialMbr(
+            lowerLeft=SpatialMbr.make_point(35.3070, -94.6393),
+            upperRight=SpatialMbr.make_point(35.5104, -94.1226),
+        ),
     )
 
+    # Prep the dataset
+    dataset = api.dataset(datasetName=primary_dataset)
+
     # Get a scene cursor using the filter
-    scene_cursor = api.dataset(datasetName="corona2").scenes(sceneFilter=scene_filter)
+    scene_cursor = dataset.scenes(sceneFilter=scene_filter)
 
     # Capture only those available for download
     scenes = scene_cursor.downloadable
 
     # Collect more scenes using the scene cursor
-    while len(scenes) < 3:
+    while scene_cursor.has_more:
         scenes += scene_cursor.next().downloadable
 
     # Enqueue the scenes
